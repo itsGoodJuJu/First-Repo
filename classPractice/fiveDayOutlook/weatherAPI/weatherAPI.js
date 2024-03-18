fc6cb08b1add4c31c14d0aab77acad3c3eb5276b
COR Coding <corcoding@LAPTOP-N1N1CVP7.localdomain>
2024-03-18 16:03:49 -0400

    weather API updates

--- classPractice/fiveDayOutlook/weatherAPI/weatherAPI.js
+++ classPractice/fiveDayOutlook/weatherAPI/weatherAPI.js
@@ -27,6 +27,7 @@ function onloadCall() {
             let cityName = data.location.name + ", " + data.location.region;
             let weatherDate = data.forecast.forecastday[i].date;
             let conditionImg = data.forecast.forecastday[i].day.condition.icon;
+            // let newConditionImg = conditionImg.replace("//", "https://");
             let highTemp = "High of: " + data.forecast.forecastday[i].day.maxtemp_f;
             let lowTemp = "Low of: " + data.forecast.forecastday[i].day.mintemp_f;
             let winds = "Wind Speed: " + data.forecast.forecastday[i].day.maxwind_mph + " mph";
@@ -44,6 +45,8 @@ function onloadCall() {
             let cityNameTag = document.createElement("p");
             let weatherDateTag = document.createElement("p");
             let conditionImgTag = document.createElement("img");
+            // newConditionImg.src = conditionImg;
+            // newConditionImg.setAttribute('src', iconURL);
             let highTempTag = document.createElement("p");
             let lowTempTag = document.createElement("p");
             let windsTag = document.createElement("p");
